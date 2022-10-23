9aaf74053c3c7bce3bf69a4a7eb6e210b7d90255
ttokutake <tadatoshi.tokutake@quipper.com>
2022-10-23 19:25:59 +0900

    Presentation is responsible for handling HTTP requests

--- part1/chapter1_layering/lib/presentation.ts
+++ part1/chapter1_layering/lib/presentation.ts
@@ -1,9 +1,11 @@
 import { Crew, Domain } from './domain.ts';
 
 export class Presentation {
-  constructor(private domain: Domain){}
+  constructor(private domain: Domain) {
+    this.handler = this.handler.bind(this);
+  }
 
-  async render(): Promise<string> {
+  async handler(_request: Request): Promise<Response> {
     const crews = await this.domain.doComplexThings();
 
     const content = crews
@@ -12,13 +14,20 @@ export class Presentation {
       })
       .join('');
 
-    return `<html>
-  <title>Layering</title>
-  <body>
-    <ul>
-      ${content}
-    </ul>
-  </body>
-</html>`;
+    const html = `
+      <html>
+        <title>Layering</title>
+        <body>
+          <ul>
+            ${content}
+          </ul>
+        </body>
+      </html>
+    `;
+
+    return new Response(html, {
+      status: 200,
+      headers: { "content-type": "text/html; charset=utf-8" },
+    });
   }
 }
--- part1/chapter1_layering/main.ts
+++ part1/chapter1_layering/main.ts
@@ -9,14 +9,5 @@ const port = 8080;
 const domain = new Domain(DataSource);
 const presentation = new Presentation(domain);
 
-const handler = async (_request: Request): Promise<Response> => {
-  const html = await presentation.render();
-
-  return new Response(html, {
-    status: 200,
-    headers: { "content-type": "text/html; charset=utf-8" },
-  });
-};
-
 console.log(`HTTP webserver running. Access it at: http://localhost:8080/`);
-await serve(handler, { port });
+await serve(presentation.handler, { port });
