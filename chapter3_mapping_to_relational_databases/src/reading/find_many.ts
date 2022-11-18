8d94c312a7dda60c3ad321f74db8430d88a31f0e
ttokutake <tadatoshi.tokutake@quipper.com>
2022-11-18 20:46:20 +0900

    findMany

---
+++ chapter3_mapping_to_relational_databases/src/reading/find_many.ts
@@ -0,0 +1,59 @@
+import { client } from "../postgres_client.ts";
+
+interface Row {
+  id: number;
+  name: string;
+  bounty: bigint;
+}
+
+export class CrewGateway {
+  private id = 0;
+
+  constructor(
+    public name: string,
+    public bounty: bigint,
+  ) {}
+
+  getId(): number {
+    return this.id;
+  }
+
+  async insert(): Promise<void> {
+    await client.queryArray`
+      INSERT INTO crews (name, bounty)
+      VALUES (${this.name}, ${this.bounty})
+    `;
+  }
+
+  static async findManyInGoodWay(ids: number[]): Promise<CrewGateway[]> {
+    const { rows } = await client.queryObject<Row>(`
+      SELECT id, name, bounty
+      FROM crews
+      WHERE id IN (${ids.join(",")})
+    `);
+    const crewGateways = rows.map((row: Row) => {
+      const crewGateway = new CrewGateway(row.name, row.bounty);
+      crewGateway.id = row.id;
+      return crewGateway;
+    });
+    return crewGateways;
+  }
+
+  static findManyInBadWay(ids: number[]): Promise<CrewGateway[]> {
+    const crewGateways = ids.map(async (id: number) => {
+      const { rows: [row] } = await client.queryObject<Row>`
+        SELECT id, name, bounty
+        FROM crews
+        WHERE id = ${id}
+        LIMIT 1
+      `;
+      if (!row) {
+        throw new Error("Record Not Found");
+      }
+      const crewGateway = new CrewGateway(row.name, row.bounty);
+      crewGateway.id = row.id;
+      return crewGateway;
+    });
+    return Promise.all(crewGateways);
+  }
+}
---
+++ chapter3_mapping_to_relational_databases/test/reading/find_many_test.ts
@@ -0,0 +1,59 @@
+import {
+  afterAll,
+  afterEach,
+  assertEquals,
+  beforeAll,
+  beforeEach,
+  describe,
+  it,
+} from "../../dev_deps.ts";
+
+import { CrewGateway } from "../../src/reading/find_many.ts";
+import { createTable, dropTable, truncateTable } from "../test_helper.ts";
+
+async function insertData() {
+  const data = [
+    { name: "Luffy", bounty: BigInt(1_500_000_000) },
+    { name: "Zoro", bounty: BigInt(320_000_000) },
+  ];
+  for (const { name, bounty } of data) {
+    const crewGateway = new CrewGateway(name, bounty);
+    await crewGateway.insert();
+  }
+}
+
+describe("CrewFinder", () => {
+  beforeAll(async () => {
+    await createTable();
+  });
+  afterAll(async () => {
+    await dropTable();
+  });
+
+  beforeEach(async () => {
+    await insertData();
+  });
+  afterEach(async () => {
+    await truncateTable();
+  });
+
+  it("findManyInGoodWay", async () => {
+    const crewGateways = await CrewGateway.findManyInGoodWay([1, 2]);
+
+    assertEquals(crewGateways[0].name, "Luffy");
+    assertEquals(crewGateways[0].bounty, BigInt(1_500_000_000));
+
+    assertEquals(crewGateways[1].name, "Zoro");
+    assertEquals(crewGateways[1].bounty, BigInt(320_000_000));
+  });
+
+  it("findManyInBadWay", async () => {
+    const crewGateways = await CrewGateway.findManyInBadWay([1, 2]);
+
+    assertEquals(crewGateways[0].name, "Luffy");
+    assertEquals(crewGateways[0].bounty, BigInt(1_500_000_000));
+
+    assertEquals(crewGateways[1].name, "Zoro");
+    assertEquals(crewGateways[1].bounty, BigInt(320_000_000));
+  });
+});
