fba081cc0e80995edf2bc288bc68bf8d26b3c02c
ttokutake <tadatoshi.tokutake@quipper.com>
2022-11-18 18:23:28 +0900

    Finder Class

---
+++ chapter3_mapping_to_relational_databases/src/reading/finder.ts
@@ -0,0 +1,63 @@
+import { client } from "../postgres_client.ts";
+
+export class Crew {
+  constructor(
+    private id: number,
+    public name: string,
+    public bounty: bigint,
+    public ranking: number,
+  ) {}
+
+  getId(): number {
+    return this.id;
+  }
+
+  async insert(): Promise<void> {
+    await client.queryArray`
+      INSERT INTO crews (name, bounty)
+      VALUES (${this.name}, ${this.bounty})
+    `;
+  }
+
+  isDanger(): boolean {
+    return this.bounty >= 1_000_000_000;
+  }
+}
+
+export interface PopularityVoteService {
+  getRanking: (id: number) => Promise<number>;
+}
+
+interface Row {
+  id: number;
+  name: string;
+  bounty: bigint;
+}
+
+export class CrewFinder {
+  constructor(
+    private popularityVoteService: PopularityVoteService,
+  ) {}
+
+  async find(id: number): Promise<Crew> {
+    const { rows: [row] } = await client.queryObject<Row>`
+      SELECT id, name, bounty
+      FROM crews
+      WHERE id = ${id}
+      LIMIT 1
+    `;
+    if (!row) {
+      throw new Error("Record Not Found");
+    }
+
+    const ranking = await this.popularityVoteService.getRanking(row.id);
+
+    const crewGateway = new Crew(
+      row.id,
+      row.name,
+      row.bounty,
+      ranking,
+    );
+    return crewGateway;
+  }
+}
---
+++ chapter3_mapping_to_relational_databases/test/reading/finder_test.ts
@@ -0,0 +1,52 @@
+import {
+  afterAll,
+  afterEach,
+  assertEquals,
+  beforeAll,
+  beforeEach,
+  describe,
+  it,
+} from "../../dev_deps.ts";
+
+import {
+  Crew,
+  CrewFinder,
+  PopularityVoteService,
+} from "../../src/reading/finder.ts";
+import { createTable, dropTable, truncateTable } from "../test_helper.ts";
+
+async function insertData() {
+  const crew = new Crew(0, "Luffy", BigInt(1_500_000_000), 0);
+  await crew.insert();
+}
+
+const PopularityVoteServiceStub: PopularityVoteService = class {
+  static getRanking(_id: number): Promise<number> {
+    return Promise.resolve(1);
+  }
+};
+
+describe("CrewFinder", () => {
+  beforeAll(async () => {
+    await createTable();
+  });
+  afterAll(async () => {
+    await dropTable();
+  });
+
+  beforeEach(async () => {
+    await insertData();
+  });
+  afterEach(async () => {
+    await truncateTable();
+  });
+
+  it("find", async () => {
+    const crewFinder = new CrewFinder(PopularityVoteServiceStub);
+    const crew = await crewFinder.find(1);
+
+    assertEquals(crew.name, "Luffy");
+    assertEquals(crew.bounty, BigInt(1_500_000_000));
+    assertEquals(crew.ranking, 1);
+  });
+});
