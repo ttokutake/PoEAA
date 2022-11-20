c18ca3bf2c01957076d0096d1522ac621d3d0c4a
ttokutake <tadatoshi.tokutake@quipper.com>
2022-11-20 20:06:20 +0900

    Metadata Mapping

---
+++ chapter3_mapping_to_relational_databases/src/using_metadata/metadata_mapping.ts
@@ -0,0 +1,86 @@
+import { client } from "../postgres_client.ts";
+
+const config = {
+  "crew": {
+    table: "crews",
+    fields: [
+      { name: "name", column: "name" },
+      { name: "bounty", column: "bounty" },
+    ],
+  },
+  "special_move": {
+    table: "special_moves",
+    fields: [
+      { name: "name", column: "name" },
+      { name: "crewId", column: "crew_id" },
+    ],
+  },
+};
+
+interface ConfigField {
+  name: string;
+  column: string;
+}
+
+abstract class Base {
+  protected static table: string;
+  protected static fields: ConfigField[];
+
+  protected static idField = { name: "id", column: "id" };
+  protected _id = 0;
+
+  [index: string]: unknown;
+
+  constructor(...values: unknown[]) {
+    (<typeof Base> this.constructor).fields.forEach((field, index) => {
+      this[field.name] = values[index];
+    });
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  async insert(): Promise<void> {
+    const self = <typeof Base> this.constructor;
+    const columnNames = self.fields.map(({ column }) => column);
+    const placeholders = columnNames.map((_v, i) => `$${i + 1}`);
+    const values = self.fields.map(({ name }) => this[name]);
+
+    await client.queryArray(
+      `
+        INSERT INTO ${self.table} (${columnNames.join(",")})
+        VALUES (${placeholders.join(",")})
+      `,
+      values,
+    );
+  }
+
+  static async find(id: unknown): Promise<Base> {
+    const columnNames = this.fields.map(({ column }) => column);
+    const { rows: [row] } = await client.queryArray(`
+      SELECT ${this.idField.column}, ${columnNames.join(",")}
+      FROM ${this.table}
+      WHERE ${this.idField.column} = ${id}
+      LIMIT 1
+    `);
+    if (!row) {
+      throw new Error("Record Not Found");
+    }
+    const valuesWithoutId = row.slice(1);
+    // @ts-ignore: I don't know how to specify concrete class except "this"
+    const instance = new this(...valuesWithoutId);
+    instance._id = row[0] as number;
+    return instance;
+  }
+}
+
+export class Crew extends Base {
+  protected static table = config.crew.table;
+  protected static fields: ConfigField[] = config.crew.fields;
+}
+
+export class SpecialMove extends Base {
+  protected static table = config.special_move.table;
+  protected static fields: ConfigField[] = config.special_move.fields;
+}
---
+++ chapter3_mapping_to_relational_databases/test/using_metadata/metadata_mapping_test.ts
@@ -0,0 +1,56 @@
+import {
+  afterAll,
+  afterEach,
+  assertEquals,
+  beforeAll,
+  beforeEach,
+  describe,
+  it,
+} from "../../dev_deps.ts";
+
+import {
+  Crew,
+  SpecialMove,
+} from "../../src/using_metadata/metadata_mapping.ts";
+import {
+  createCrewsTable,
+  createSpecialMovesTable,
+  dropTable,
+  truncateCrewsTable,
+} from "../test_helper.ts";
+
+async function insertData() {
+  const crew = new Crew("Luffy", BigInt(1_500_000_000));
+  await crew.insert();
+
+  const specialMove = new SpecialMove("Gum-Gum Pistol", 1);
+  await specialMove.insert();
+}
+
+describe("Base", () => {
+  beforeAll(async () => {
+    await createCrewsTable();
+    await createSpecialMovesTable();
+  });
+  afterAll(async () => {
+    await dropTable();
+  });
+
+  beforeEach(async () => {
+    await insertData();
+  });
+  afterEach(async () => {
+    await truncateCrewsTable();
+  });
+
+  it("find", async () => {
+    const crew = await Crew.find(1);
+
+    assertEquals(crew.name, "Luffy");
+    assertEquals(crew.bounty, BigInt(1_500_000_000));
+
+    const specialMove = await SpecialMove.find(1);
+
+    assertEquals(specialMove.name, "Gum-Gum Pistol");
+  });
+});
