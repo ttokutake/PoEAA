2b4784370456d2a51795e1fbe99a7f4a468084b5
ttokutake <tadatoshi.tokutake@quipper.com>
2022-11-20 13:41:18 +0900

    Single Table Inheritance

---
+++ chapter3_mapping_to_relational_databases/src/inheritance/single_table_inheritance.ts
@@ -0,0 +1,93 @@
+import { client } from "../postgres_client.ts";
+
+interface PeopleRow {
+  id: number;
+  name: string;
+  role: string | null;
+  rank: string | null;
+}
+
+abstract class Person {
+  protected static type = "";
+  protected id = 0;
+
+  constructor(
+    public name: string,
+  ) {}
+
+  getId(): number {
+    return this.id;
+  }
+}
+
+export class Pirate extends Person {
+  protected static type = "pirate";
+
+  constructor(
+    public name: string,
+    public role: string,
+  ) {
+    super(name);
+  }
+
+  async insert(): Promise<void> {
+    await client.queryArray`
+      INSERT INTO people (name, type, role)
+      VALUES (${this.name}, ${Pirate.type}, ${this.role})
+    `;
+  }
+
+  static async find(id: number): Promise<Pirate> {
+    const { rows: [row] } = await client.queryObject<PeopleRow>`
+      SELECT id, name, role
+      FROM people
+      WHERE id = ${id} AND type = ${Pirate.type}
+      LIMIT 1
+    `;
+    if (!row) {
+      throw new Error("Record Not Found");
+    }
+    if (!row.role) {
+      throw new Error('Pirate must have "role"');
+    }
+    const pirate = new Pirate(row.name, row.role);
+    pirate.id = row.id;
+    return pirate;
+  }
+}
+
+export class Marine extends Person {
+  protected static type = "marine";
+
+  constructor(
+    public name: string,
+    public rank: string,
+  ) {
+    super(name);
+  }
+
+  async insert(): Promise<void> {
+    await client.queryArray`
+      INSERT INTO people (name, type, rank)
+      VALUES (${this.name}, ${Marine.type}, ${this.rank})
+    `;
+  }
+
+  static async find(id: number): Promise<Marine> {
+    const { rows: [row] } = await client.queryObject<PeopleRow>`
+      SELECT id, name, rank
+      FROM people
+      WHERE id = ${id} AND type = ${Marine.type}
+      LIMIT 1
+    `;
+    if (!row) {
+      throw new Error("Record Not Found");
+    }
+    if (!row.rank) {
+      throw new Error('Pirate must have "rank"');
+    }
+    const marine = new Marine(row.name, row.rank);
+    marine.id = row.id;
+    return marine;
+  }
+}
