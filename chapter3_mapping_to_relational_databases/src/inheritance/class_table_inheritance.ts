29cf83abda7c9c386f1746330b2b5922860d44a7
ttokutake <tadatoshi.tokutake@quipper.com>
2022-11-20 14:27:11 +0900

    Class Table Inheritance

---
+++ chapter3_mapping_to_relational_databases/src/inheritance/class_table_inheritance.ts
@@ -0,0 +1,117 @@
+import { client } from "../postgres_client.ts";
+
+abstract class Person {
+  protected id = 0;
+
+  constructor(
+    public name: string,
+  ) {}
+
+  getId(): number {
+    return this.id;
+  }
+
+  protected async insert(): Promise<number> {
+    await client.queryArray`
+      INSERT INTO people (name)
+      VALUES (${this.name})
+    `;
+    const id = await this.findIdByName();
+    return id;
+  }
+
+  protected async findIdByName(): Promise<number> {
+    const { rows: [row] } = await client.queryObject<{ id: number }>`
+      SELECT id
+      FROM people
+      WHERE name = ${this.name}
+      LIMIT 1
+    `;
+    if (!row) {
+      throw new Error("Record Not Found");
+    }
+    return row.id;
+  }
+}
+
+interface PirateRow {
+  id: number;
+  name: string;
+  role: string;
+}
+
+export class Pirate extends Person {
+  constructor(
+    public name: string,
+    public role: string,
+  ) {
+    super(name);
+  }
+
+  async insert(): Promise<number> {
+    const id = await super.insert();
+    await client.queryArray`
+      INSERT INTO pirates (person_id, role)
+      VALUES (${id}, ${this.role})
+    `;
+    return id;
+  }
+
+  static async find(id: number): Promise<Pirate> {
+    const { rows: [row] } = await client.queryObject<PirateRow>`
+      SELECT id, name, role
+      FROM people
+      INNER JOIN pirates
+        ON person_id = id
+      WHERE id = ${id}
+      LIMIT 1
+    `;
+    if (!row) {
+      throw new Error("Record Not Found");
+    }
+    const pirate = new Pirate(row.name, row.role);
+    pirate.id = row.id;
+    return pirate;
+  }
+}
+
+interface MarineRow {
+  id: number;
+  name: string;
+  rank: string;
+}
+
+export class Marine extends Person {
+  constructor(
+    public name: string,
+    public rank: string,
+  ) {
+    super(name);
+  }
+
+  async insert(): Promise<number> {
+    const id = await super.insert();
+    await client.queryArray`
+      INSERT INTO marines (person_id, rank)
+      VALUES (${id}, ${this.rank})
+    `;
+    return id;
+  }
+
+  static async find(id: number): Promise<Marine> {
+    const { rows: [row] } = await client.queryObject<MarineRow>`
+      SELECT id, name, rank
+      FROM people
+      INNER JOIN marines
+        ON person_id = id
+      WHERE id = ${id}
+      LIMIT 1
+    `;
+    if (!row) {
+      throw new Error("Record Not Found");
+    }
+    const marine = new Marine(row.name, row.rank);
+    marine.id = row.id;
+    return marine;
+  }
+}
---
+++ chapter3_mapping_to_relational_databases/test/inheritance/class_table_inheritance_test.ts
@@ -0,0 +1,84 @@
+import {
+  afterAll,
+  afterEach,
+  assertEquals,
+  beforeAll,
+  beforeEach,
+  describe,
+  it,
+} from "../../dev_deps.ts";
+
+import {
+  Marine,
+  Pirate,
+} from "../../src/inheritance/class_table_inheritance.ts";
+import { client } from "../../src/postgres_client.ts";
+
+async function insertData() {
+  const pirate = new Pirate("Jimbei", "Helmsman");
+  await pirate.insert();
+  const marine = new Marine("Smoker", "Vice Admiral");
+  await marine.insert();
+}
+
+describe("Person", () => {
+  beforeAll(async () => {
+    await client.queryArray`
+      CREATE TABLE IF NOT EXISTS people (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(256) NOT NULL,
+        UNIQUE(name)
+      )
+    `;
+    await client.queryArray`
+      CREATE TABLE IF NOT EXISTS pirates (
+        person_id INTEGER PRIMARY KEY,
+        role VARCHAR(256) NOT NULL,
+        CONSTRAINT fk_people
+          FOREIGN KEY(person_id)
+            REFERENCES people(id)
+      )
+    `;
+    await client.queryArray`
+      CREATE TABLE IF NOT EXISTS marines (
+        person_id INTEGER PRIMARY KEY,
+        rank VARCHAR(256) NOT NULL,
+        CONSTRAINT fk_people
+          FOREIGN KEY(person_id)
+            REFERENCES people(id)
+      )
+    `;
+  });
+  afterAll(async () => {
+    await client.queryArray`
+      DROP TABLE IF EXISTS pirates
+    `;
+    await client.queryArray`
+      DROP TABLE IF EXISTS marines
+    `;
+    await client.queryArray`
+      DROP TABLE IF EXISTS people
+    `;
+  });
+
+  beforeEach(async () => {
+    await insertData();
+  });
+  afterEach(async () => {
+    await client.queryArray`
+      TRUNCATE TABLE people RESTART IDENTITY CASCADE
+    `;
+  });
+
+  it("find", async () => {
+    const pirate = await Pirate.find(1);
+
+    assertEquals(pirate.name, "Jimbei");
+    assertEquals(pirate.role, "Helmsman");
+
+    const marine = await Marine.find(2);
+
+    assertEquals(marine.name, "Smoker");
+    assertEquals(marine.rank, "Vice Admiral");
+  });
+});
