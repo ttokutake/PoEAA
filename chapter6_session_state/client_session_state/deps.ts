c3adfea504e52c4c96a3512b284cf91a2daa45a4
ttokutake <tadatoshi.tokutake@gmail.com>
2023-03-14 22:01:05 +0900

    Client Session State

--- chapter6_session_state/client_session_state/deps.ts
+++ chapter6_session_state/client_session_state/deps.ts
@@ -1,2 +1,2 @@
-export { serve } from "https://deno.land/std@0.159.0/http/server.ts";
-export { dirname, fromFileUrl } from "https://deno.land/std@0.57.0/path/mod.ts";
+export { serve } from "https://deno.land/std@0.179.0/http/server.ts";
+export { dirname, fromFileUrl } from "https://deno.land/std@0.179.0/path/mod.ts";
--- chapter6_session_state/client_session_state/src/main.ts
+++ chapter6_session_state/client_session_state/src/main.ts
@@ -6,11 +6,35 @@ const __dirname = dirname(fromFileUrl(import.meta.url));
 const indexHtml = await Deno.readTextFile(
   `${__dirname}/index.html`,
 );
+const nextHtml = await Deno.readTextFile(
+  `${__dirname}/next.html`,
+);
+const confirmHtml = await Deno.readTextFile(
+  `${__dirname}/confirm.html`,
+);
+const completeHtml = await Deno.readTextFile(
+  `${__dirname}/complete.html`,
+);
 
 function handler(request: Request): Response {
-  const _url = new URL(request.url);
+  const url = new URL(request.url);
 
-  return new Response(indexHtml, {
+  let html;
+  switch (url.pathname) {
+    case '/next':
+      html = nextHtml;
+      break;
+    case '/confirm':
+      html = confirmHtml;
+      break;
+    case '/complete':
+      html = completeHtml;
+      break;
+    default:
+      html = indexHtml;
+  }
+
+  return new Response(html, {
     status: 200,
     headers: { "content-type": "text/html; charset=utf-8" },
   });
